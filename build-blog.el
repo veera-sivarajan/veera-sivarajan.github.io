8892eb668ae1fe879e35ee8be04b50f5f6a3dce7
Veera <sveera.2001@gmail.com>
2022-05-19 20:13:28 -0400

    Adding RSS feed failed

--- build-blog.el
+++ build-blog.el
@@ -73,43 +73,6 @@ PROJECT: `posts in this case."
         ((eq style 'tree) (file-name-nondirectory (directory-file-name entry)))
         (t entry)))
 
-(defun rw/org-rss-publish-to-rss (plist filename pub-dir)
-  "Publish RSS with PLIST, only when FILENAME is 'rss.org'.
-PUB-DIR is when the output will be placed."
-  (if (equal "rss.org" (file-name-nondirectory filename))
-      (org-rss-publish-to-rss plist filename pub-dir)))
-
-
-(defun rw/format-rss-feed (title list)
-  "Generate RSS feed, as a string.
-TITLE is the title of the RSS feed.  LIST is an internal
-representation for the files to include, as returned by
-`org-list-to-lisp'.  PROJECT is the current project."
-  (concat "#+TITLE: " title "\n\n"
-          (org-list-to-subtree list 1 '(:icount "" :istart ""))))
-
-
-(defun rw/format-rss-feed-entry (entry style project)
-  "Format ENTRY for the RSS feed.
-ENTRY is a file name.  STYLE is either 'list' or 'tree'.
-PROJECT is the current project."
-  (cond ((not (directory-name-p entry))
-         (let* ((file (org-publish--expand-file-name entry project))
-                (title (org-publish-find-title entry project))
-                (date (format-time-string "%Y-%m-%d" (org-publish-find-date entry project)))
-                (link (concat (file-name-sans-extension entry) ".html")))
-           (with-temp-buffer
-             (insert (format "* [[file:%s][%s]]\n" file title))
-             (org-set-property "RSS_PERMALINK" link)
-             (org-set-property "PUBDATE" date)
-             (insert-file-contents file)
-             (buffer-string))))
-        ((eq style 'tree)
-         ;; Return only last subdir.
-         (file-name-nondirectory (directory-file-name entry)))
-        (t entry)))
-
-
 (setq org-publish-project-alist
       `(("posts"
          :base-directory "~/Projects/Blog/local/"
@@ -148,23 +111,5 @@ PROJECT is the current project."
          :publishing-directory "~/Projects/Blog/docs/imgs/"
          :publishing-function org-publish-attachment
          :recursive t)
-        ("rss"
-         :base-directory "~/Projects/Blog/local/"
-         :base-extension "org"
-         :recursive nil
-         :exclude ,(regexp-opt '("rss.org" "index.org" "404.org"))
-         :publishing-function rw/org-rss-publish-to-rss
-         :publishing-directory "~/Projects/Blog/docs/"
-         :rss-extension "xml"
-         :html-link-home "https://veera.app/"
-         :html-link-use-abs-url t
-         :html-link-org-files-as-html t
-         :auto-sitemap t
-         :sitemap-filename "rss.org"
-         :sitemap-title "Core Dump RSS"
-         :sitemap-style list
-         :sitemap-sort-files anti-chronologically
-         :sitemap-function rw/format-rss-feed
-         :sitemap-format-entry rw/format-rss-feed-entry)
-        ("all" :components ("posts" "about" "css"  "images" "rss"))))
+        ("all" :components ("posts" "about" "css"  "images"))))
 
